6940e93b6dd950202b38ce8f6c56e5985cbdc02e
Siphesihle <siphesihledhlamini384gmail.com>
2022-02-23 21:46:09 +0200

    playRound function created

--- rps.js
+++ rps.js
@@ -1,1 +1,40 @@
 
+let playerScore = 0;
+let computerScore = 0;
+let roundWinner = '';
+
+function playRound(playerSelection, computerSelection) { 
+    if (playerSelection === computerSelection) {
+        roundWinner = 'tie';
+    }
+    if (
+        (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
+        (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') ||
+        (playerSelection === 'PAPER' && computerSelection === 'ROCK')
+    ) {
+        playerScore++
+        roundWinner = 'player'
+    }
+    if (
+        (computerSelection === 'ROCK' && playerSelection === 'SSCISSORS') || 
+        (computerSelection === 'ROCK' && playerSelection === 'PAPER') || 
+        (computerSelection === 'ROCK' && playerSelection === 'ROCK')
+    ) {
+        computerScore++
+        roundWinner = 'computer'
+    }
+    updateScoreMessage(roundWinner, playerSelection, computerSelection)
+}
+
+function getRandomChoice() {
+    let randomNumber = Math.floor(Math.random() * 3)
+    switch (randomNumber) {
+      case 0:
+        return 'ROCK'
+      case 1:
+        return 'PAPER'
+      case 2:
+        return 'SCISSORS'
+    }
+  }
+  
