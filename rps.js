8f2aad04161fd0b056b1459fcad10dc74e174d3c
Siphesihle <siphesihledhlamini384gmail.com>
2022-04-08 09:46:37 +0200

    funcion declarations

--- rps.js
+++ rps.js
@@ -1,76 +1,23 @@
+//this js file is inclusive of a function called game that will build the logic
 
-let playerScore = 0;
-let computerScore = 0;
-let roundWinner = '';
+//main game function declaration
+const game = () => {
+  //declare block scope variables
+  let playerScore = 0;
+  let computerScore = 0;
+  let moves = 0;
 
-function playRound(playerSelection, computerSelection) { 
-    if (playerSelection === computerSelection) {
-        roundWinner = 'tie';
-    }
-    if (
-        (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
-        (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') ||
-        (playerSelection === 'PAPER' && computerSelection === 'ROCK')
-    ) {
-        playerScore++
-        roundWinner = 'thePlayer'
-    }
-    if (
-        (computerSelection === 'ROCK' && playerSelection === 'SSCISSORS') || 
-        (computerSelection === 'ROCK' && playerSelection === 'PAPER') || 
-        (computerSelection === 'ROCK' && playerSelection === 'ROCK')
-    ) {
-        computerScore++
-        roundWinner = 'theComputer'
-    }
-    updateScoreMessage(roundWinner, playerSelection, computerSelection)
-}
+  //function that specifies buttons that will allows user to play
+  const playGame = () => {
+    const rockBtn = document.querySelector(".rock");
+    const paperBtn = document.querySelector(".paper");
+    const scissorBtn = document.querySelector(".scissor");
 
-function getRandomChoice() {
-    let randomNumber = Math.floor(Math.random() * 3)
-    switch (randomNumber) {
-      case 0:
-        return 'ROCK'
-      case 1:
-        return 'PAPER'
-      case 2:
-        return 'SCISSORS'
-    }
+    //parameters(options) we have as a player
+    const playerOptions = [rockBtm, paperBtn, scissorBtn];
+    //parameters(options) the computer has
+    const computerOptions = ["rock", "paper", "scissor"];
+
   }
-  
-function isGameOver() { 
-    return playerScore === 5 || computerScore === 5
 }
-
-function updateScore() {
-    if (roundWinner === 'tie') {
-      scoreInfo.textContent = "It's a tie!"
-    } else if (roundWinner === 'player') {
-      scoreInfo.textContent = 'You won!'
-    } else if (roundWinner === 'computer') {
-      scoreInfo.textContent = 'You lost!'
-    }
-  
-    playerScorePara.textContent = `Player: ${playerScore}`
-    computerScorePara.textContent = `Computer: ${computerScore}`
-}
-  
-function updateScoreMessage(winner, playerSelection, computerSelection) {
-    if (winner === 'player') {
-      scoreMessage.textContent = `${capitalizeFirstLetter(
-        playerSelection
-      )} beats ${computerSelection.toLowerCase()}`
-      return
-    }
-    if (winner === 'computer') {
-      scoreMessage.textContent = `${capitalizeFirstLetter(
-        playerSelection
-      )} is beaten by ${computerSelection.toLowerCase()}`
-      return
-    }
-  
-    scoreMessage.textContent = `${capitalizeFirstLetter(
-      playerSelection
-    )} ties with ${computerSelection.toLowerCase()}`
-  }
   
